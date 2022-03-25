d28e0d9416f9d7567d52faefca87bd3774be0b69
Kasidis Chantharojwong <77087017+ckasidis@users.noreply.github.com>
2022-03-26 01:32:35 +0800

    add website icon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,16 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import Header from '../components/Header';
+import Head from 'next/head';
 
-function MyApp({ Component, pageProps }: AppProps) {
+export default function MyApp({ Component, pageProps }: AppProps) {
 	return (
 		<>
+			<Head>
+				<link rel="icon" href="/NTUTH.ico" />
+			</Head>
 			<Header />
 			<Component {...pageProps} />
 		</>
 	);
 }
-
-export default MyApp;
