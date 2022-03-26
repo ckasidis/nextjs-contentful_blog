e52093cb86a7c9ac2273168a6519d978a7e701e0
Kasidis Chantharojwong <77087017+ckasidis@users.noreply.github.com>
2022-03-27 03:14:26 +0800

    create GridSection component

---
+++ components/GridSection.tsx
@@ -0,0 +1,11 @@
+interface GridSectionProps {
+	children: React.ReactNode;
+}
+
+export default function GridSection({ children }: GridSectionProps) {
+	return (
+		<div className="relative bg-gray-50 py-16 px-4 sm:px-6 lg:px-8">
+			<div className="relative max-w-7xl mx-auto">{children}</div>
+		</div>
+	);
+}
