6ebe16b5489d9e08e1f514df920f6663abe98ea5
Kasidis Chantharojwong <77087017+ckasidis@users.noreply.github.com>
2022-03-31 02:46:59 +0800

    add page transition

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,36 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import { motion } from 'framer-motion';
 import Header from '../components/Header';
 import Head from 'next/head';
 
-export default function MyApp({ Component, pageProps }: AppProps) {
+export default function MyApp({ Component, pageProps, router }: AppProps) {
+	const pageTransition = {
+		hidden: {
+			opacity: 0,
+		},
+		visible: {
+			opacity: 1,
+			transition: {
+				duration: 0.2,
+			},
+		},
+	};
 	return (
 		<>
 			<Head>
 				<link rel="icon" href="/NTUTH.ico" />
 			</Head>
 			<Header />
-			<Component {...pageProps} />
+			<motion.div
+				className="app-container"
+				key={router.route}
+				variants={pageTransition}
+				initial="hidden"
+				animate="visible"
+			>
+				<Component {...pageProps} />
+			</motion.div>
 		</>
 	);
 }
